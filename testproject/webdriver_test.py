59114fb89185f7e25c0ccdbe50b3a46522eb1c12
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-11 16:13:48 +0200

    Update webdriver_test.py
    kikommenteltem, hatha ez a baj

--- testproject/webdriver_test.py
+++ testproject/webdriver_test.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+""" from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
@@ -9,4 +9,4 @@ elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
-driver.close()
+driver.close()"""
