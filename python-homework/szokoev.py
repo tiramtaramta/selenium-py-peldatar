4b262bac19d95feb838aa82ab80a92bd21059d9b
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 14:17:52 +0200

    Create szokoev.py
    ez még nincs kész

---
+++ python-homework/szokoev.py
@@ -0,0 +1,11 @@
+"""
+Szökőév?
+Készíts függvényt, amelyik adott évszámról eldönti, hogy az szökőév-e.
+Szökőév minden negyedik,
+nem szökőév minden századik, mégis az minden 400-adik. (2000-ben ezért volt szökőév.)
+A függvény visszatérési értéke legyen logikai típusú!
+Tipp( % maradekos osztas operátor)
+Írj programot, amelyik a felhasználótól évszámokat kér, és mindegyikre kiírja, hogy szökőév-e!
+Használd az előbb megírt függvényt!
+Például: ? 2005 Nem szökőév. ? 2000 Szökőév. ? 1980 Szökőév. ? 1900 Nem szökőév.
+A megoldást egy szokoev.py nevű file-ban kell beadnod."""
