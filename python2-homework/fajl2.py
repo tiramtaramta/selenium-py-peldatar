f92a9b86020babeddea77b6257912a1f7a1c1fdd
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 16:08:59 +0200

    Create fajl2.py
    kesz

---
+++ python2-homework/fajl2.py
@@ -0,0 +1,6 @@
+"""
+Olvasd be a fájlt, tárold a sorokat listában, majd írd ki a lista tartalmát egy sorban!
+"""
+with open("adat.txt", "r") as file:
+    adat_list = file.readlines()
+    print(adat_list)
