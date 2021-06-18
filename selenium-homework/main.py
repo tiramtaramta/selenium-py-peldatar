17588129a9f95d4d2924004fa8434844acf9bd33
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 14:19:44 +0200

    Create main.py
    kesz

---
+++ selenium-homework/main.py
@@ -0,0 +1,9 @@
+"""
+Készíts egy bmodul nevezetű python modult.
+Hozz benne létre egy változót és tárolj benne egy értéket.
+Egy másik modulban, mondjuk main modul, importáld be a bmodul-ból a változót és használd fel egy kiiratásban.
+A megoldást egy bmodul.py és egy main.py nevű fileban kell beadnod.
+"""
+import bmodul
+
+print(int(bmodul.valt) * 3)
