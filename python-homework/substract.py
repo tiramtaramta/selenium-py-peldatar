5ed85e442b2c91148df9ed47d9d2a03be572d188
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 13:33:04 +0200

    Create substract.py
    kesz

---
+++ python-homework/substract.py
@@ -0,0 +1,7 @@
+"""
+Kérjünk be két számot, és írjuk ki a különgségüket. Használd az input() és print() beépített függvényeket.
+"""
+user_input1 = int(input("Kérlek adj meg egy számot: "))
+user_input2 = int(input("Kérlek adj meg egy  újabb számot: "))
+
+print(user_input1 - user_input2)
