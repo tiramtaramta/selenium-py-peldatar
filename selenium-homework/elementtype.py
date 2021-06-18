ec2a57c2d27cbb411847d017fad59ae99cfef668
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 14:25:43 +0200

    Create elementtype.py
    kesz

---
+++ selenium-homework/elementtype.py
@@ -0,0 +1,32 @@
+"""
+Készíts egy Python alkalmazást ami selenium-ot használ. Indítsd el lokálisan a selenium-py-peldatar alkalmazást.
+A program töltse be a példatárból az http://localhost:9999/trickyelements.html oldalt.
+Használj id lokátort és keressd ki az elemenekt egyesével. A legelső olyan elemre ami button típusú kattints rá
+és ellenőrizd, hogy a helyes szöveg jelenik-e meg az elemek listája alatt.
+A megoldást egy elementtype.py nevű fileban kell beadnod.
+"""
+from selenium import webdriver
+
+# PATH = "C:\\Windows\\chromedriver.exe" # Másik megoldás, ami elegánsabb
+# browser = webdriver.Chrome(PATH)
+
+driver = webdriver.Chrome()
+
+
+try:
+    driver.get("http://localhost:9999/trickyelements.html")
+
+    elements = driver.find_elements_by_xpath('//*[@id]')
+    # print(type(elements))
+
+    for element in elements:
+        # print(element.tag_name)
+        if element == driver.find_element_by_tag_name("button"):
+            element.click()
+            # print(element.tag_name)
+            result = driver.find_element_by_id("result")
+            print(result.text)
+            break
+
+finally:
+    driver.close()
