d0cd47d9c0fa92f28d1a601232dfbbb4c433a3d6
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 14:23:35 +0200

    Create seleniumex.py
    nincs kesz

---
+++ selenium-homework/seleniumex.py
@@ -0,0 +1,6 @@
+"""
+Készíts egy Python alkalmazást ami selenium-ot használ. Nyisson meg egy Chrome böngészöt és töltsön be egy tetszőleges weblapot az Internetről. 
+Az oldalon probáld megtalálni a <div id="nemletezik"></div> mezőt. A lényeg, hogy hibát dogjon a driver.find_by_id függvény hívás. 
+Feladatot, hogy kezed le ezt a hibát és írj ki valami emberileg olvasható üzenetet. 
+Extra feladatként készíts egy saját függvényt, ami bármilyen find_by_id lokátor hívásnál lekezeli a nemlétező elem tipusú hibát.
+"""
