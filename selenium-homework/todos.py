24a01260f31f15af5fc28c8e38f14f4a39cebf14
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-18 14:27:19 +0200

    Create todos.py
    nincs kesz

---
+++ selenium-homework/todos.py
@@ -0,0 +1,7 @@
+"""
+A program töltse be a példatárból az http://localhost:9999/todo.html oldalt.
+A feladatod, hogy kigyűjtsd az összes jelenleg aktív Todo bejegyzést.
+Ha lehet akkor ezt minnél kevesebb selenium lokátorral old meg.
+(Tökéletes megoldáshoz csak egy darab find_by hívásra van szükség).
+A megoldást egy todos.py nevű fileban kell beadnod.
+"""
