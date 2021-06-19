bea185387cfcd2d49cbc288a0ac75e5af87e4766
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 12:21:45 +0200

    Update fajl1.py
    kesz

--- python2-homework/fajl1.py
+++ python2-homework/fajl1.py
@@ -2,4 +2,4 @@
 Olvasd be a fájlt, és írd ki a tartalmát egy sorba, úgy, hogy nem tárolod el a szöveget, hanem minden sort azonnal kiírsz!
 """
 with open("adat.txt", "r") as file:
-    print(file.readlines())
+    print(* [s[:-1] for s in file.readlines()], sep = " ")
