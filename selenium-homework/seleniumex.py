68064592f9c915b5d5d8338c78ac4f88614b200a
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 13:51:49 +0200

    Update seleniumex.py
    kesz

--- selenium-homework/seleniumex.py
+++ selenium-homework/seleniumex.py
@@ -4,3 +4,22 @@ Az oldalon probáld megtalálni a <div id="nemletezik"></div> mezőt. A lényeg,
 Feladatot, hogy kezed le ezt a hibát és írj ki valami emberileg olvasható üzenetet. 
 Extra feladatként készíts egy saját függvényt, ami bármilyen find_by_id lokátor hívásnál lekezeli a nemlétező elem tipusú hibát.
 """
+from selenium import webdriver
+
+url_name = input("Kérlek adj meg egy pontos URL-címet: ")
+element_name = input("Kérlek adj meg egy ID-nevet: ")
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get(url_name)
+
+    nems = driver.find_elements_by_id(element_name)
+
+    if len(nems) > 0:
+        print(f"Van {element_name} nevű elem az oldalon.")
+    else:
+        print(f"Nincs {element_name} nevű elem az oldalon.")
+
+finally:
+    driver.close()
