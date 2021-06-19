fb74fc51b0325d5c11cf856ca2e0f63af0efc5f5
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 12:48:20 +0200

    Update fajl4.py
    file.close

--- python2-homework/fajl4.py
+++ python2-homework/fajl4.py
@@ -3,7 +3,7 @@ Olvasd be a fájlt, tárold a sorokat listában, majd írd ki a lista tartalmát
 """
 with open("adat.txt", "r") as file:
     adat_list = file.readlines()
-
+    file.close()
 
 with open("adat3.txt", "w") as file:
     for element in adat_list:
