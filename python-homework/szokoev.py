97dd35e14a2d5177cdae98ba2efb4b7c3b6f03a5
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 08:56:31 +0200

    Update szokoev.py
    kesz

--- python-homework/szokoev.py
+++ python-homework/szokoev.py
@@ -9,3 +9,21 @@ Tipp( % maradekos osztas operátor)
 Használd az előbb megírt függvényt!
 Például: ? 2005 Nem szökőév. ? 2000 Szökőév. ? 1980 Szökőév. ? 1900 Nem szökőév.
 A megoldást egy szokoev.py nevű file-ban kell beadnod."""
+
+
+def szokoev(a):
+    if a % 4 == 0 and a % 400 == 0:
+        return True
+    elif a % 4 == 0 and a % 100 == 0 and a % 400 != 0:
+        return False
+    elif a % 4 == 0:
+        return True
+    else:
+        return False
+
+
+for i in range(5):
+    if szokoev(int(input("Kérlek adj meg egy évszámot: "))):
+        print("Szökőév.")
+    else:
+        print("Nem szökőév.")
