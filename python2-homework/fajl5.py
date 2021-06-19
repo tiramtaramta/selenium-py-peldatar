a9c2d3e3d10b00ff7ef116cdc9d6147778576312
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 12:56:38 +0200

    Update fajl5.py
    kesz

--- python2-homework/fajl5.py
+++ python2-homework/fajl5.py
@@ -1,3 +1,11 @@
 """
 Olvasd be a fájlt, és írd ki a tartalmát egy másik fájlba, úgy, hogy nem tárolod el a szöveget, hanem minden sort azonnal kiírsz!
 """
+with open('adat.txt', 'r') as in_file, \
+     open('adat4.txt', 'w') as out_file:
+
+    for element in in_file.readlines():
+        out_file.write(element)
+
+in_file.close()
+out_file.close()
