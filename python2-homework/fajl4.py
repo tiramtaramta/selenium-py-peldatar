2347aaaf192f5cc5877f333b0810d7cae35871ee
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 12:37:59 +0200

    Update fajl4.py
    kesz

--- python2-homework/fajl4.py
+++ python2-homework/fajl4.py
@@ -1,3 +1,11 @@
 """
 Olvasd be a fájlt, tárold a sorokat listában, majd írd ki a lista tartalmát így, ahogy beolvastad, soronként egy szóval egy másik fájlba!
 """
+with open("adat.txt", "r") as file:
+    adat_list = file.readlines()
+
+
+with open("adat3.txt", "w") as file:
+    for element in adat_list:
+        file.write(element)
+    file.close()
