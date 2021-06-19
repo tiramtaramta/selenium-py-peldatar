cbe8f0c14d28b5027ef60b748be63f55ac3ee8a9
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-19 14:26:25 +0200

    Update todos.py
    kesz

--- selenium-homework/todos.py
+++ selenium-homework/todos.py
@@ -5,3 +5,19 @@ Ha lehet akkor ezt minnél kevesebb selenium lokátorral old meg.
 (Tökéletes megoldáshoz csak egy darab find_by hívásra van szükség).
 A megoldást egy todos.py nevű fileban kell beadnod.
 """
+
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("http://localhost:9999/todo.html")
+
+    done_falses = driver.find_elements_by_xpath('//span[@class="done-false"]')
+
+    for done_false in done_falses:
+        elements = done_false.text
+        print(elements)
+
+finally:
+    driver.close()
