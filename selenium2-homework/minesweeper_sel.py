7aab14e4365905baab037988f6579c0c1f58e59e
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-20 10:03:20 +0200

    Create minesweeper_sel.py
    nincs kesz

---
+++ selenium2-homework/minesweeper_sel.py
@@ -0,0 +1,7 @@
+"""
+Készíts egy Python alkalmazást ami selenium-ot használ. Készíts egy Python alkalmazást ami selenium-ot használ. 
+Indítsd el lokálisan a selenium-py-peldatar alkalmazást. A program töltse be a példatárból az http://localhost:9999/minesweeper-game.html oldalt. 
+A feladat egyszerű éld túl az aknamezőt. A selenium python programnak kell automatikusan végigjétszania a játékot. 
+A megoldáshoz használhattsz más könyvtárakat is pl: https://github.com/madewokherd/mines
+A megoldást egy minesweeper_sel.py nevű fileban kell beadnod.
+"""
