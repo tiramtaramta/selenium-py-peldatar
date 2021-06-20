17458cdf334374e5b56291ef636d11ba7b961be2
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-20 08:24:52 +0200

    Create linkek.py
    nincs kesz

---
+++ selenium2-homework/linkek.py
@@ -0,0 +1,6 @@
+"""
+Készíts egy Python alkalmazást ami selenium-ot használ. Indítsd el lokálisan a selenium-py-peldatar alkalmazást. 
+A program töltse be a példatárból az http://localhost:9999 oldalt. Lokátorral keresd ki az összes linket az oldalról. 
+Tárold a memóriában egy listában az összes linket. A list tartalmát írd ki egy fájlba. Minden link egy új sorba kerüljön. 
+A kimenetre írd ki hány linket találtál
+"""
