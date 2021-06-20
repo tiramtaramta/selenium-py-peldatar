b31fce84198a3f94ae8524b099d8369eafda2e08
Vaska Gergely <35167572+tiramtaramta@users.noreply.github.com>
2021-06-20 08:58:16 +0200

    Update linkek.py
    kesz

--- selenium2-homework/linkek.py
+++ selenium2-homework/linkek.py
@@ -4,3 +4,39 @@ A program töltse be a példatárból az http://localhost:9999 oldalt. Lokátorr
 Tárold a memóriában egy listában az összes linket. A list tartalmát írd ki egy fájlba. Minden link egy új sorba kerüljön. 
 A kimenetre írd ki hány linket találtál
 """
+from selenium import webdriver
+
+PATH = "C:\\Windows\\chromedriver.exe"
+
+browser = webdriver.Chrome(PATH)
+
+
+def file_write():
+    with open("links.txt", "w") as file:
+        for link in links_list:
+            file.write(link + "\n")
+        file.close()
+
+
+def count_links():
+    list_len = len(links_list)
+    print(list_len)
+
+
+try:
+    browser.get("http://localhost:9999")
+
+    links = browser.find_elements_by_xpath("//a[@href]")
+    links_list = []
+
+    if len(links) > 0:
+        for link in links:
+            links_list.append(link.get_attribute("href"))
+    else:
+        print("Nincsenek linkek az oldalon.")
+
+    file_write()
+    count_links()
+
+finally:
+    browser.quit()
