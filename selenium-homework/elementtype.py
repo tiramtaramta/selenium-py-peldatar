f3ece846deacca30750bb4cc90322fe2701f55da
Vaska Gergely <vaska.gergely@gmail.com>
2021-06-29 11:30:27 +0200

    megoldva egyenkenti id kikeresessel

--- selenium-homework/elementtype.py
+++ selenium-homework/elementtype.py
@@ -30,3 +30,25 @@ try:
 
 finally:
     driver.close()
+
+# # solution 2
+# try:
+#     driver.get("http://localhost:9999/trickyelements.html")
+#
+#     element1 = driver.find_element_by_id("element1")
+#     element2 = driver.find_element_by_id("element2")
+#     element3 = driver.find_element_by_id("element3")
+#     element4 = driver.find_element_by_id("element4")
+#     element5 = driver.find_element_by_id("element5")
+#
+#     res = driver.find_element_by_id("result")
+#
+#     tag_list = [element1, element2, element3, element4, element5]
+#     for elem in tag_list:
+#         if elem.tag_name == "button":
+#             elem.click()
+#             if res.text == f"{elem.text} vas clicked":
+#                 print(f"{elem.text} was clicked and text was right")
+#             break
+# finally:
+#     driver.quit()
