e81995a0f8bdcc842789cd233873d8a6d21b3fe9
Vaska Gergely <vaska.gergely@gmail.com>
2021-06-29 08:13:15 +0200

    print javitva es a szavak sorbarendezve

--- python2-homework/charsandord.py
+++ python2-homework/charsandord.py
@@ -1,11 +1,11 @@
 """
 Szavak előfordulási gyakorisága
-Állapítsuk meg, hogy az alábbi szövegben (TEXT) melyik szó hányszor fordul elő s írassuk ki az eredményt a következő formában: szó_1 előfordulások_száma_1 szó_2 előfordulások_száma_2 ...
+Állapítsuk meg, hogy az alábbi szövegben (TEXT) melyik szó hányszor fordul elő s írassuk ki az eredményt a következő formában:
+szó_1 előfordulások_száma_1 szó_2 előfordulások_száma_2 ...
 
-Az eredményt úgy írassuk ki, hogy a lista szavak szerint rendezve legyen. Minden szó kisbetűsen szerepeljen, vagyis pl. a 'The' és 'the' szavak azonos szónak számítanak.
-
+Az eredményt úgy írassuk ki, hogy a lista szavak szerint rendezve legyen. Minden szó kisbetűsen szerepeljen, vagyis pl.
+a 'The' és 'the' szavak azonos szónak számítanak.
 Használjuk az str.split() függvényt (paraméter nélkül) a whitespace karakterek eltávolítására.
-
 Az egyes szavakhoz kapcsolódó írásjelekkel (pont, vessző, idézőjel, stb.) nem kell most foglalkozni.
 """
 
@@ -14,6 +14,7 @@ from collections import Counter
 
 with open("text.txt", "r") as file:
     text_list = file.read().lower().split()
+    text_list.sort()
     dict_list = dict(Counter(text_list))
     for k, v in dict_list.items():
-        print(k, v)
+        print(f"szó_{k}, előfordulások száma_{v}")
