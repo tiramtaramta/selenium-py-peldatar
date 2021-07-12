1e6bd807a4d19d63a629a716af7890297f916a30
Vaska Gergely <vaska.gergely@gmail.com>
2021-07-12 19:04:59 +0200

    kiegeszitve

---
+++ selenium2-homework/draganddrophard.py
@@ -0,0 +1,5 @@
+"""
+Készíts egy Python alkalmazást ami selenium-ot használ. Indítsd el lokálisan a selenium-py-peldatar alkalmazást.
+A program töltse be a példatárból az http://localhost:9999/dragndrop2.html oldalt.
+A feladatod, hogy a todo oszlobpól átrakd az összes kártyát a doing oszlopba.
+"""
