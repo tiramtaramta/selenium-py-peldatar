01a2c8e8b14168e1b185e89390e38d39d9149770
Vaska Gergely <vaska.gergely@gmail.com>
2021-07-19 19:12:11 +0200

    scrollcatloader done (a cat mappaba mentve)

--- selenium2-homework/scrollcatloader.py
+++ selenium2-homework/scrollcatloader.py
@@ -6,16 +6,13 @@ A fájlneve legyen a következő {sorszam}_{cat_id} ahol a sorszám alatt azt é
 és cat_id meg az azonosító amit szolgáltató ad. A {} jelek ne legyenek benne a fájlnévben.
 (ez a feladat majdnem ugyan az, mint az előző feladat, csakhogy nincs load more gomb.)
 """
+
 import time
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.common.keys import Keys
 
 PATH = "C:\\Windows\\chromedriver.exe"
-URL = "http://localhost:9999/loadmore.html"
+URL = "http://localhost:9999/scrolltoload.html"
 
 browser = webdriver.Chrome(PATH)
 
@@ -24,14 +21,23 @@ try:
     js = "window.scrollTo(0,2000)"
     browser.execute_script(js)
     pic_num = 0
+    images = []
 
-    images = browser.find_elements_by_xpath("//div[@class='image']")
+    while True:
+        images = browser.find_elements_by_xpath("//div[@class='image']")
+        if len(images) >= 20:
+            break
+        time.sleep(2)
 
-    for image in images:
+        html = browser.find_element_by_tag_name('html')
+        html.send_keys(Keys.PAGE_DOWN)
+        time.sleep(3)
+
+    for image in images[0:20]:
         pic_num += 1
         pic_name = f"{pic_num}_{image.find_element_by_tag_name('p').text[8:]}.jpg"
         url = image.find_element_by_tag_name("img").get_attribute("src")
-        with open(f'cats/{pic_name}', 'wb') as file:
+        with open(f'cat/{pic_name}', 'wb') as file:
             file.write(image.screenshot_as_png)
         print(url)
         print(pic_name)
